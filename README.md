eda71403f9dfd5782a939621ed2d68ba9a316cfc
sophieruetschi <sophie.ruetschi@ufl.edu>
2025-01-27 09:10:16 -0500

    flutter application skeleton

---
+++ README.md
@@ -0,0 +1,3 @@
+# Kubera Frontend (Flutter)
+
+download and install project tools using: https://docs.flutter.dev/get-started/install
---
+++ flutter_kubera/android/app/src/main/kotlin/com/example/flutter_kubera/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_kubera
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
