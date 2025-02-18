d64de0fcfd24c82647e6088fe181b199c6c52eac
sarahperez <perezsarah14@gmail.com>
2025-02-18 12:58:35 -0500

    test server call to ensure server-client connection

--- flutter_kubera/lib/src/ui/tabs/dashboard/dashboard_screen.dart
+++ flutter_kubera/lib/src/ui/tabs/dashboard/dashboard_screen.dart
@@ -1,12 +1,43 @@
 import 'package:flutter/material.dart';
 
-class DashboardScreen extends StatelessWidget {
+import '../../../models/test.dart';
+import '../../../services/flask_service.dart';
+
+class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
 
+  @override
+  _DashboardScreenState createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  late Future<Test> futureTest;
+
+  @override
+  void initState() {
+    super.initState();
+    futureTest = FlaskService().fetchTest();
+  }
+
+  // Sample code: https://github.com/toliksadovnichiy/Flutter-lab4/blob/08aadb19b0c20f5e439dc20173b5f6b03e4e4b1c/lib/parse_json_widget.dart
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Dashboard")),
+      body: Center(
+        child: FutureBuilder<Test>(
+          future: futureTest,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Text(snapshot.data!.message);
+            } else if (snapshot.hasError) {
+              return Text('${snapshot.error}');
+            }
+
+            return const CircularProgressIndicator();
+          },
+        ),
+      ),
     );
   }
 }
