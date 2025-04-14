4382417b8b4e4ca3ea4f02ffd488290475ae762f
sarahperez <perezsarah14@gmail.com>
2025-04-14 14:52:12 -0400

    check if user is logged in

--- flutter_kubera/lib/src/ui/tabs/shopping_list/shopping_list_screen.dart
+++ flutter_kubera/lib/src/ui/tabs/shopping_list/shopping_list_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_kubera/src/models/shopping_list.dart';
 import 'package:flutter_kubera/src/services/flask_service.dart';
+import 'package:flutter_kubera/src/ui/tabs/settings/auth_provider.dart';
 import 'package:flutter_kubera/src/ui/tabs/shopping_list/shopping_list_details.dart';
 import 'package:flutter_kubera/src/ui/tabs/shopping_list/create_shopping_list_dialog.dart';
+import 'package:provider/provider.dart';
 
 class ShoppingListScreen extends StatefulWidget {
   const ShoppingListScreen({Key? key}) : super(key: key);
@@ -13,11 +15,15 @@ class ShoppingListScreen extends StatefulWidget {
 
 class _ShoppingListScreenState extends State<ShoppingListScreen> {
   late Future<List<ShoppingList>> _shoppingListsFuture;
+  late bool userLoggedIn;
 
   @override
   void initState() {
     super.initState();
-    _shoppingListsFuture = FlaskService().getUsersShoppingLists(context);
+    if (context.read<AuthState>().isAuthorized) {
+      _shoppingListsFuture = FlaskService().getUsersShoppingLists(context);
+    }
+    userLoggedIn = context.read<AuthState>().isAuthorized;
   }
 
   Future<void> _refreshShoppingLists() async {
@@ -57,73 +63,73 @@ class _ShoppingListScreenState extends State<ShoppingListScreen> {
         title: const Text('Shopping Lists'),
       ),
       body: Column(
-        children: [
+        children: userLoggedIn
+            ? [
           Expanded(
             child: FutureBuilder<List<ShoppingList>>(
               future: _shoppingListsFuture,
               builder: (context, snapshot) {
                 if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(child: CircularProgressIndicator());
+            return const Center(child: CircularProgressIndicator());
                 } else if (snapshot.hasError) {
-                  return Center(
-                    child: Text('Error: ${snapshot.error}'),
-                  );
+            return Center(
+              child: Text('Error: ${snapshot.error}'),
+            );
                 } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                  return const Center(
-                    child: Text('No shopping lists found.'),
-                  );
+            return const Center(
+              child: Text('No shopping lists found.'),
+            );
                 } else {
-                  final shoppingLists = snapshot.data!;
-                  return ListView.builder(
-                    padding: const EdgeInsets.all(16.0),
-                    itemCount: shoppingLists.length,
-                    itemBuilder: (context, index) {
-                      final shoppingList = shoppingLists[index];
-                      return Dismissible(
-                        key: Key(shoppingList.id),
-                        direction: DismissDirection.endToStart,
-                        background: Container(
-                          color: Colors.red,
-                          alignment: Alignment.centerRight,
-                          padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                          child: const Icon(Icons.delete, color: Colors.white),
-                        ),
-                        onDismissed: (direction) {
-                          _deleteShoppingList(shoppingList.id);
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(content: Text('${shoppingList.listName} deleted')),
-                          );
-                        },
-                        child: Card(
-                          margin: const EdgeInsets.only(bottom: 16.0),
-                          elevation: 3,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(12),
-                          ),
-                          child: ListTile(
-                            title: Text(
-                              shoppingList.listName,
-                              style: const TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            onTap: () {
-                              // Navigate to the detailed view of the shopping list
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) =>
-                                      ShoppingListDetails(shoppingList: shoppingList),
-                                ),
-                                ).then((_) => _refreshShoppingLists()
-                              );
-                            },
-                          ),
-                        ),
-                      );
-                    },
-                  );
+            final shoppingLists = snapshot.data!;
+            return ListView.builder(
+              padding: const EdgeInsets.all(16.0),
+              itemCount: shoppingLists.length,
+              itemBuilder: (context, index) {
+                final shoppingList = shoppingLists[index];
+                return Dismissible(
+                  key: Key(shoppingList.id),
+                  direction: DismissDirection.endToStart,
+                  background: Container(
+              color: Colors.red,
+              alignment: Alignment.centerRight,
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: const Icon(Icons.delete, color: Colors.white),
+                  ),
+                  onDismissed: (direction) {
+              _deleteShoppingList(shoppingList.id);
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text('${shoppingList.listName} deleted')),
+              );
+                  },
+                  child: Card(
+              margin: const EdgeInsets.only(bottom: 16.0),
+              elevation: 3,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: ListTile(
+                title: Text(
+                  shoppingList.listName,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                onTap: () {
+                  // Navigate to the detailed view of the shopping list
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                builder: (context) =>
+                    ShoppingListDetails(shoppingList: shoppingList),
+                    ),
+                  ).then((_) => _refreshShoppingLists());
+                },
+              ),
+                  ),
+                );
+              },
+            );
                 }
               },
             ),
@@ -135,13 +141,24 @@ class _ShoppingListScreenState extends State<ShoppingListScreen> {
               children: [
                 const Text('Add List'),
                 IconButton(
-                  onPressed: _showCreateShoppingListDialog,
-                  icon: const Icon(Icons.add_task),
+            onPressed: _showCreateShoppingListDialog,
+            icon: const Icon(Icons.add_task),
                 ),
               ],
             ),
           ),
-        ],
+              ]
+            : [
+            const Center(
+              child: Center(
+                child: Text(
+                'Log in to access shopping lists',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+          ],
       ),
     );
   }
